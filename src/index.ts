19c43d7b153c198fbb3676ce83c2e18a3413a7a9
superlucky84 <superlucky84@gmail.com>
2026-02-01 02:02:13 +0900

    docs first commit

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-export { normalizeGithubUrl, validateCommitSha, normalizeSkillPath } from "./validators";
-export { detectSkillValueType } from "./value";
-export { SKILLPACK_HEADER, SKILLREF_HEADER } from "./constants";
-export { createSkillpackFromDir } from "./skillpack";
-export { createSkillrefValue, parseSkillrefValue } from "./skillref";
-export { loadSkillrefToDir } from "./skillref";
-export { resolveSaveInput } from "./input";
-export { CtxbinError, formatError } from "./errors";
-export { safeChmod } from "./chmod";
--- src/value.ts
+++
@@ -1,9 +0,0 @@
-import { SKILLPACK_HEADER, SKILLREF_HEADER } from "./constants";
-
-export type SkillValueType = "skillpack" | "skillref" | "string";
-
-export function detectSkillValueType(value: string): SkillValueType {
-  if (value.startsWith(SKILLPACK_HEADER)) return "skillpack";
-  if (value.startsWith(SKILLREF_HEADER)) return "skillref";
-  return "string";
-}
